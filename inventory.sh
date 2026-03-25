2fdf6ea3e6db9f4638308f346d4aba5808c035ac
Rudiger-8 <lekoundjifernand@gmail.com>
2026-03-26 00:10:33 +0100

    modification du fichier syskit

---
+++ inventory.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+show_os() {
+echo "Système d'exploitation :"
+uname -o
+}
+
+show_kernel() {
+echo "Version du noyau :"
+uname -r
+}
+
+show_cpu() {
+echo "Processeur :"
+lscpu | grep "Model name"
+}
+
+show_ram() {
+echo "Mémoire RAM :"
+free -h
+}
+
+show_disk() {
+echo "Espace disque :"
+df -h
+}
+
+show_ip() {
+echo "Adresse IP :"
+hostname -I
+}
+
+echo "===== INVENTORY ====="
+
+show_os
+show_kernel
+show_cpu
+show_ram
+show_disk
+show_ip
