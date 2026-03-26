aa6e657efd443ff89afeb6156330ed42b5b5d451
Rudiger-8 <lekoundjifernand@gmail.com>
2026-03-26 13:25:26 +0100

    modification des erreurs du code

---
+++ syskit/modules/rapport.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rapport="reports/report.txt"
+
+echo "===== RAPPORT SYSKIT =====" > "$rapport"
+echo "Date : $(date)" >> "$rapport"
+echo "" >> "$rapport"
+
+echo "===== INFORMATIONS SYSTEME =====" >> "$rapport"
+uname -a >> "$rapport"
+echo "" >> "$rapport"
+
+echo "===== SAUVEGARDES REALISEES =====" >> "$rapport"
+
+if [ -f logs/backup.log ]
+then
+    cat logs/backup.log >> "$rapport"
+else
+    echo "Aucune sauvegarde" >> "$rapport"
+fi
+
+echo "" >> "$rapport"
+
+echo "===== NETTOYAGE =====" >> "$rapport"
+
+if [ -f logs/cleanup.log ]
+then
+    cat logs/cleanup.log >> "$rapport"
+else
+    echo "Aucun nettoyage effectué" >> "$rapport"
+fi
+
+echo "" >> "$rapport"
+
+echo "Rapport généré dans $rapport"
---
+++ syskit/syskit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script principal de SysKit
+# Permet d'exécuter différentes commandes selon l'argument
+commande="$1"
+
+if test "$commande"
+then
+echo "Erreur : aucune commande donnée"
+echo "Usage : ./syskit.sh {inventory|backup|cleanup|report}"
+exit 1
+fi
+
+case "$commande" in
+
+inventory)
+bash modules/inventory.sh
+;;
+
+backup)
+bash modules/backup.sh "$2"
+;;
+
+cleanup)
+bash modules/cleanup.sh "$2"
+;;
+
+report)
+bash modules/report.sh
+;;
+
+*)
+echo "Commande inconnue"
+;;
+
+esac
